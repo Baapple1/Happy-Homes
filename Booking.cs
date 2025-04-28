685db91b60b9f88bb036370a10b67961ac9196ec
Baapple1 <n6hodge@gmail.com>
2025-04-28 15:09:16 +0100

    Add project files.

---
+++ Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy_Homes
+{
+    public class Booking
+    {
+        // Viewing details, inc:
+        // Date & Time
+        // Property
+        // Staff administering
+    }
+}
---
+++ Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy_Homes
+{
+    public class Customer
+    {
+        //Forename
+        //Surname
+        //Email
+        //Postal
+        //Phone
+    }
+}
