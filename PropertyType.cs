25f790a9b6e95e9fea13a5eb7520b5751025dd44
Baapple1 <n6hodge@gmail.com>
2025-05-03 13:55:23 +0100

    PropertyType

---
+++ PropertyType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy_Homes
+{
+    public enum PropertyType
+    {
+        Detached,
+        SemiDetached,
+        Bungalow,
+        Flat,
+        Terrace,
+        Enterprise
+    }
+}
