bb922fbb70b5f9587c4d4230b6883b9b23dbf758
Baapple1 <n6hodge@gmail.com>
2025-05-04 10:58:54 +0100

    PropertiesFilter

--- Property.cs
+++ Property.cs
@@ -11,26 +11,55 @@ namespace Happy_Homes
     public class Property
     {
         // Attributes
-        public static Dictionary<PropertyType, string> properties { get; set; }
+        public static Dictionary<PropertyType, List<string>> properties { get; set; } = new();
 
         // Methods
         public static void ViewProperties()
         {
             foreach (var property in properties)
             {
-                Console.WriteLine(@$"{property.Value}: {property.Key}
+                foreach (var address in property.Value)
+                {
+                    Console.WriteLine(@$"{address}: {property.Key}
 -------------------------------");
-            }    
+                }
+            }
         }
         public static void AddProperty(PropertyType type, string address)
         {
-            // Add the Property to the Dictionary
-            properties.Add(type, address);
+            if (!properties.ContainsKey(type))
+            {
+                properties[type] = new List<string>();
+            }
 
+            properties[type].Add(address);
         }
         public static bool ValidateProperty(string address)
         {
-            return properties.Any(property => property.Value == address);   // Checks Properties Dictionary for matching address
+            return properties.Any(property => property.Value.Contains(address));
+        }
+        public static void FilterProperties(string requestedAddress)
+        {
+            bool found = false;
+            foreach (var property in properties)
+            {
+                foreach (var address in property.Value)
+                {
+                    if (address == requestedAddress)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($@"{address}: {property.Key}
+
+Press any key to continue");
+                        found = true;
+                    }
+                }
+            }
+            if (!found)
+            {
+                Console.Clear();
+                Console.WriteLine("Not Found");
+            }
         }
     }
 }
